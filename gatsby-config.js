d7376fa73da854c7f743ea5f7b7d5616b4243617
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2021-09-29 21:02:26 -0500

    daniellrr

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
