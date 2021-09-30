1a7abe891f1b85019086d4e131e28a64c8b22773
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2021-09-29 20:39:42 -0500

    terminado1

--- src/components/restaurantes/RestaurantCard.js
+++ src/components/restaurantes/RestaurantCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import urlSlug from 'url-slug'
 import './restaurantes.css'
-import star from '../../../public/icons/star.svg'
+
 
 const RestaurantCard = ({calificacion,imagen,seo,nombre,categoria,ubicaciones}) => {
     return (
@@ -10,7 +10,6 @@ const RestaurantCard = ({calificacion,imagen,seo,nombre,categoria,ubicaciones}) 
         <Link to={urlSlug(nombre)}  className="restaurante">
         <div className="imagen">
                     <div className="start">
-                        <img src={star} alt="rating" className="rating"/>
                         <h3>{calificacion}</h3>
                     </div>
                     <img src={imagen} alt={seo} />
